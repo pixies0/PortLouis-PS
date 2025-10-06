bbaa51fb74f279f0b6e562933b0307fb20de2723
João Pedro Silva Cunha <jpcunha5@gmail.com>
2025-10-05 22:40:34 -0300

    💡 Documentação do  projeto desenvolvido.

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -5,9 +5,9 @@ export const config = {
   port: parseInt(process.env.PORT || '3000', 10),
   db: {
     host: process.env.DB_HOST || '127.0.0.1',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '123456',
-    database: process.env.DB_NAME || 'contatos_db',
+    user: process.env.DB_USER || 'seu_usuario',
+    password: process.env.DB_PASSWORD || 'sua_senha',
+    database: process.env.DB_NAME || 'seu_banco_de_dados',
     port: parseInt(process.env.DB_PORT || '3306', 10)
   }
 };
